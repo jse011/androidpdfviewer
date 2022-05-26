5897b877fbb6a27d445e536f6da50333781c44e0
Jse <jose.arias@jefa.io>
2022-05-26 11:08:16 -0500

    Corrections

--- app/src/main/java/com/rajat/pdfviewer/MainActivity.kt
+++ app/src/main/java/com/rajat/pdfviewer/MainActivity.kt
@@ -1,25 +1,33 @@
 package com.rajat.pdfviewer
 
 import android.Manifest
+import android.content.Context
 import android.content.pm.PackageManager
+import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
+import android.os.Build
 import android.os.Bundle
+import android.text.TextUtils
+import android.view.View
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import com.rajat.pdfviewer.databinding.ActivityMainBinding
+import java.io.File
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
-
+    private var fileUrl: String? = null
+    private var permissionGranted: Boolean? = false
 
     private val requiredPermissionList = arrayOf(
         Manifest.permission.WRITE_EXTERNAL_STORAGE,
         Manifest.permission.READ_EXTERNAL_STORAGE
     )
 
-    private var download_file_url = "https://github.com/afreakyelf/Pdf-Viewer/files/5856345/AA.pdf"
+    private var download_file_url = "https://www.trabajo.gob.pe/archivos/file/guias/MODELO_DE_CONTRATO_DE_TRABAJO_SUJETO_A_MODALIDAD_.pdf"
     var per = 0f
     private val PERMISSION_CODE = 4040
 
@@ -33,6 +41,110 @@ class MainActivity : AppCompatActivity() {
             if (checkAndRequestPermission())
                 launchPdf()
         }
+                init()
+    }
+
+    private fun init() {
+        fileUrl = download_file_url
+        if (false) {
+            initPdfViewerWithPath(this.fileUrl)
+        } else {
+            if (checkInternetConnection(this)) {
+                loadFileFromNetwork(this.fileUrl)
+            } else {
+                Toast.makeText(
+                    this,
+                    "No Internet Connection. Please Check your internet connection.",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+        }
+    }
+
+    private fun loadFileFromNetwork(fileUrl: String?) {
+        initPdfViewer(
+            fileUrl,
+            PdfViewerActivity.engine
+        )
+    }
+
+    private fun initPdfViewer(fileUrl: String?, engine: PdfEngine) {
+        if (TextUtils.isEmpty(fileUrl)) onPdfError()
+
+        //Initiating PDf Viewer with URL
+        try {
+            binding.pdfView.initWithUrl(
+                fileUrl!!,
+                PdfQuality.NORMAL,
+                engine
+            )
+        } catch (e: Exception) {
+            onPdfError()
+        }
+
+        enableDownload()
+
+    }
+
+    private fun checkInternetConnection(context: Context): Boolean {
+        var result = 0 // Returns connection type. 0: none; 1: mobile data; 2: wifi
+        val cm = context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager?
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            cm?.run {
+                cm.getNetworkCapabilities(cm.activeNetwork)?.run {
+                    when {
+                        hasTransport(NetworkCapabilities.TRANSPORT_WIFI) -> {
+                            result = 2
+                        }
+                        hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) -> {
+                            result = 1
+                        }
+                        hasTransport(NetworkCapabilities.TRANSPORT_VPN) -> {
+                            result = 3
+                        }
+                    }
+                }
+            }
+        } else {
+            cm?.run {
+                cm.activeNetworkInfo?.run {
+                    when (type) {
+                        ConnectivityManager.TYPE_WIFI -> {
+                            result = 2
+                        }
+                        ConnectivityManager.TYPE_MOBILE -> {
+                            result = 1
+                        }
+                        ConnectivityManager.TYPE_VPN -> {
+                            result = 3
+                        }
+                    }
+                }
+            }
+        }
+        return result != 0
+    }
+
+    private fun initPdfViewerWithPath(filePath: String?) {
+        if (TextUtils.isEmpty(filePath)) onPdfError()
+
+        //Initiating PDf Viewer with URL
+        try {
+
+            val file = if (PdfViewerActivity.isFromAssets)
+                com.rajat.pdfviewer.util.FileUtils.fileFromAsset(this, filePath!!)
+            else File(filePath!!)
+
+            binding.pdfView.initWithFile(
+                file,
+                PdfQuality.NORMAL
+            )
+
+        } catch (e: Exception) {
+            onPdfError()
+        }
+
+        enableDownload()
     }
 
     private fun launchPdf() {
@@ -42,6 +154,58 @@ class MainActivity : AppCompatActivity() {
                 "Title", "dir",true
             )
         )
+    }
+
+    private fun checkPermissionOnInit() {
+        if (ContextCompat.checkSelfPermission(
+                this,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE
+            ) === PackageManager.PERMISSION_GRANTED
+        ) {
+            permissionGranted = true
+        }
+    }
+
+    private fun enableDownload() {
+
+        checkPermissionOnInit()
+
+        binding.pdfView.statusListener = object : PdfRendererView.StatusCallBack {
+            override fun onDownloadStart() {
+                true.showProgressBar()
+            }
+
+            override fun onDownloadProgress(
+                progress: Int,
+                downloadedBytes: Long,
+                totalBytes: Long?
+            ) {
+                //Download is in progress
+            }
+
+            override fun onDownloadSuccess() {
+                false.showProgressBar()
+            }
+
+            override fun onError(error: Throwable) {
+                onPdfError()
+            }
+
+            override fun onPageChanged(currentPage: Int, totalPage: Int) {
+                //Page change. Not require
+            }
+
+        }
+    }
+
+    private fun onPdfError() {
+        Toast.makeText(this, "Pdf has been corrupted", Toast.LENGTH_SHORT).show()
+        true.showProgressBar()
+        finish()
+    }
+
+    private fun Boolean.showProgressBar() {
+        binding.progressBar.visibility = if (this) View.VISIBLE else View.GONE
     }
 
     private fun checkAndRequestPermission(): Boolean {
